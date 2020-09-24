eb03eba21a640086cff187a71215251f3ed3b625
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-09-24 21:24:25 +0300

    Update libmx

---
+++ libmx/src/03_list/mx_list_at.c
@@ -0,0 +1,18 @@
+//
+// Created by Rostyslav Druzhchenko on 16.09.2020.
+//
+
+#include <libmx.h>
+
+t_list *mx_list_at(t_list *list, int index) {
+    if (list == 0)
+        return 0;
+
+    int i = 0;
+    while (list) {
+        if (i == index)
+            return list;
+        list = list->next;
+    }
+    return 0;
+}
--- libmx/src/mx_printspaces.c
+++
@@ -1,7 +0,0 @@
-#include "libmx.h"
-
-void mx_printspaces(int count) {
-    if (count > 0)
-        for (int i = 0; i < count; i++)
-            mx_printchar(' ');
-}

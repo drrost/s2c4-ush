d4c6e5926fd03d93cf860453a0a164413c638115
Karyna Barinova <kbarinova@e1r1p18.unit.ua>
2020-09-25 17:05:59 +0300

    fix output

--- src/main.c
+++ src/main.c
@@ -34,7 +34,8 @@ int main(int argc, const char *argv[]) {
             break;
         if (strstr(line, "pwd") != 0)
             mx_pwd(line);
-        //printf("%c", ch);
+        else
+            printf("%c", ch);
         line[i] = ch;
         i++;
     }

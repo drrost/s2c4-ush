f65f1197f1e408752bc25b287315b37f96b9ea3f
Karyna Barinova <kbarinova@e3r5p7.unit.ua>
2020-10-07 17:00:04 +0300

    changes strjoin_free to mx_str_append func

--- src/algorithm/controller/execute/mx_which.c
+++ src/algorithm/controller/execute/mx_which.c
@@ -57,27 +57,6 @@ static int flag_parse(char **command, bool *flag) {
     return i;
 }
 
-char *mx_strjoin_free(char *s1, const char *s2) {
-    char *join = NULL;
-    int i;
-    if (s1 == NULL && s2 == NULL) 
-        return NULL;
-    else if (s1 == NULL) 
-        join = mx_strdup(s2);
-    else if (s2 == NULL)
-        join = mx_strdup(s1);
-    else {
-        join = mx_strnew(mx_strlen(s1) + mx_strlen(s2));
-        for (i = 0; i < mx_strlen(s1); i++)
-            join[i] = s1[i];
-        for (int j = 0; j < mx_strlen(s2); j++, i++)
-            join[i] = s2[j];
-        join[i] = '\0';
-    }
-    mx_strdel(&s1);
-    return join;
-}
-
 static bool mx_is_command(char *path, bool *flag, int index) {
     struct stat buf;
 
@@ -104,9 +83,9 @@ static bool find_in_path(char **ways, char *command, bool *flag) {
             a = mx_is_command(command, flag, 0);
         else {
             char *path = mx_strjoin(ways[i], "/");
-            char *buf = mx_strjoin_free(path, command);
-            a = mx_is_command(buf, flag, 0);
-            free(buf);
+            mx_str_append(&path, command);
+            a = mx_is_command(path, flag, 0);
+            //free(buf);
         }
         if (a == 1) {
             if (!flag[0])

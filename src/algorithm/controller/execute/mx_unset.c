0a0a8e26a7e01b5a51671d568e6a6a3add5ed25a
Karyna Barinova <kbarinova@e3r4p8.unit.ua>
2020-10-09 12:46:56 +0300

    finished unset

--- src/algorithm/controller/execute/mx_unset.c
+++ src/algorithm/controller/execute/mx_unset.c
@@ -4,13 +4,39 @@
 
 #include <ush.h>
 
-int mx_unset(char *arguments) {
-	if (mx_strlen(arguments)) {
+static int mx_count_el(char **arr) {
+    int i = 0;
 
-	}
-	else {
-		mx_printerr("unset: not enough arguments\n");
-		return 1;
-	}
-	return 0;
+    if (arr != NULL) {
+        while (arr[i])
+            i++;
+        return i;
+    }
+    return 0;
+}
+
+int mx_unset(char *arguments) {
+    if (mx_strlen(arguments)) {
+        char **arr = mx_strsplit(arguments, ' ');
+        for (int i = 0; i < mx_count_el(arr); i++) {
+            if (mx_isalpha(arr[i][0]) || arr[i][0] == '_')
+                unsetenv(arr[i]);
+            else if (mx_strcmp(arr[i], "-") == 0) {
+                mx_printerr("unset: not enough arguments\n");
+                return 1;
+            }
+            else {
+                mx_printerr("unset: bad option: -");
+                write(2, &arr[i][1], 1);
+                mx_printerr("\n");
+                return 1;
+            }
+        }
+        mx_del_strarr(&arr);
+    }
+    else {
+        mx_printerr("unset: not enough arguments\n");
+        return 1;
+    }
+    return 0;
 }

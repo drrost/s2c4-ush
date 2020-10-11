f5626956fda8bbcff0cee16d1934bb352d511899
Karyna Barinova <kbarinova@e3r3p3.unit.ua>
2020-10-11 14:03:56 +0300

    added not_found error case for env

--- src/algorithm/controller/execute/mx_env.c
+++ src/algorithm/controller/execute/mx_env.c
@@ -24,16 +24,22 @@ void env_i(char **arr, int i) {
     }
 }
 
-void env_u(char **arr, int i) {
+int env_not_found(const char *s) {
+    mx_printerr("env: ");
+    mx_printerr(s);
+    mx_printerr(": No such file or directory\n");
+    return 127;
+}
+
+int env_u(char **arr, int i) {
+    int status = 0;
     if (!arr[i + 2]) {
         mx_unsetenv(arr[i + 1]);
         print_env();
     }
-    else {
-        mx_printerr("env: ");
-        mx_printerr(arr[i + 2]);
-        mx_printerr(": No such file or directory\n");
-    }
+    else
+        status = env_not_found(arr[i + 2]);
+    return status;
 }
 
 static int env_flags(char **args) {
@@ -42,6 +48,9 @@ static int env_flags(char **args) {
     for (; args[i]; i++)
         for (int j = 0; args[i][j]; j++)
             if (args[i][0] == '-') {
+                if (!args[i][1] && args[i + 1]) {
+                    return env_not_found(args[i + 1]);
+                }
                 if (args[i][1] == 'P') {
                     if (!args[i + 1] && !args[i][2]) {
                         mx_printerr("env: option requires an argument -- P\n");
@@ -57,10 +66,8 @@ static int env_flags(char **args) {
                         mx_printerr("env: option requires an argument -- u\n");
                         return 0;
                     }
-                    else {
-                        env_u(args, i);
-                        return 0;
-                    }
+                    else
+                        return env_u(args, i);
                 }
                 else {
                     mx_printerr("env: illegal option -- ");
@@ -69,6 +76,8 @@ static int env_flags(char **args) {
                     return 0;
                 }
             }
+            else
+                return env_not_found(args[i]);
 
     return 0;
 } 

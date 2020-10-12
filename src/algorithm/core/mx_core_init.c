ee0249fb4f879b75017b6fbf2a06fb83f35b3947
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-10-12 17:44:18 +0300

    Add reading history from a file.

--- src/algorithm/core/mx_core_init.c
+++ src/algorithm/core/mx_core_init.c
@@ -3,9 +3,9 @@
 //
 
 #include <mx_core.h>
-#include <libmx.h>
+#include <ush.h>
 
-void mx_core_init() {
+static void init_env() {
     extern char **environ;
     char *s = *environ;
     int i = 1;
@@ -16,4 +16,27 @@ void mx_core_init() {
         mx_del_strarr(&split_arr);
         s = *(environ + i);
     }
+}
+
+char *mx_history_file() {
+    return 0;
+}
+
+static void init_history() {
+    static char *history_file_name = ".ush_history";
+    char *home_dir = mx_getenv("HOME");
+    t_path *path = mx_path_new(home_dir);
+    path->append(path, history_file_name);
+    int fd = open(path->p, O_RDONLY);
+    mx_path_del(&path);
+
+    char *line = 0;
+    while (mx_read_line(&line, 1000, '\n', fd) > 0) {
+        mx_history_add(line);
+    }
+}
+
+void mx_core_init() {
+    init_env();
+    init_history();
 }

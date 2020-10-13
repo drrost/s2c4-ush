5863aeb952cf291a06e91e04e190df99525d3ef6
nadin <nadidnails92@gmail.com>
2020-10-13 14:02:31 +0300

    fix  and error

--- src/algorithm/controller/parse/mx_error_pair.c
+++ src/algorithm/controller/parse/mx_error_pair.c
@@ -10,24 +10,24 @@ static int fiend_count(char *str, char c) {
 }
 
 void mx_error_pair(char *str) {
+    t_command *err = mx_command_new();
+
     if ((fiend_count(str, '"') % 2) != 0) {
-        mx_printerr(MX_DELIM_NO_PAIR);
-        mx_printerr("\n");
-        exit(-1);
+        err->error_text = mx_strdup(MX_DELIM_NO_PAIR);
+
     } else if ((fiend_count(str, '`') % 2) != 0) {
-        mx_printerr(MX_DELIM_NO_PAIR2);
-        mx_printerr("\n");
-        exit(-1);
+        err->error_text = mx_strdup(MX_DELIM_NO_PAIR2);
+
     }
     if (fiend_count(str, '(') != fiend_count(str, ')')) {
         if (fiend_count(str, '(') < fiend_count(str, ')')) {
-            mx_printerr(MX_DELIM_NO_PAIR3);
-            mx_printerr("\n");
-            exit(-1);
+            err->error_text = mx_strdup(MX_DELIM_NO_PAIR3);
+
         } else if (fiend_count(str, ')') < fiend_count(str, '(')) {
-            mx_printerr(MX_DELIM_NO_PAIR4);
-            mx_printerr("\n");
-            exit(-1);
+            err->error_text = mx_strdup(MX_DELIM_NO_PAIR4);
+            if (fiend_count(str, '{') != fiend_count(str, '}')) {
+                   err->error_text = mx_strdup(MX_DELIM_NO_PAIR5);
+            }
         }
     }
 }

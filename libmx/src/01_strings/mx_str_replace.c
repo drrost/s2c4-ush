603117b8a1c667043c99b9056df9fe4312d94d80
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-10-16 00:04:02 +0300

    Replace system functions with mx ones.

--- libmx/src/01_strings/mx_str_replace.c
+++ libmx/src/01_strings/mx_str_replace.c
@@ -3,7 +3,6 @@
 //
 
 #include <libmx.h>
-#include <string.h>
 
 char *
 mx_str_replace(char *target, const char *needle, const char *replacement) {
@@ -11,18 +10,18 @@ mx_str_replace(char *target, const char *needle, const char *replacement) {
     char *buffer = mx_strnew(len * 2); // Should be enough for the beginning
     char *insert_point = &buffer[0];
     const char *tmp = target;
-    size_t needle_len = strlen(needle);
-    size_t repl_len = strlen(replacement);
+    size_t needle_len = mx_strlen(needle);
+    size_t repl_len = mx_strlen(replacement);
 
     while (1) {
-        const char *p = strstr(tmp, needle);
+        const char *p = mx_strstr(tmp, needle);
         if (p == NULL) {
-            strcpy(insert_point, tmp);
+            mx_strcpy(insert_point, tmp);
             break;
         }
-        memcpy(insert_point, tmp, p - tmp);
+        mx_memcpy(insert_point, tmp, p - tmp);
         insert_point += p - tmp;
-        memcpy(insert_point, replacement, repl_len);
+        mx_memcpy(insert_point, replacement, repl_len);
         insert_point += repl_len;
         tmp = p + needle_len;
     }

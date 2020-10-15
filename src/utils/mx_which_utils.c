53855a2ad0da547e0b71af57fe69c4ee59a71ab3
Karyna Barinova <kbarinova@e1r1p8.unit.ua>
2020-10-15 19:28:38 +0300

    edited mx_file_exists

--- src/utils/mx_which_utils.c
+++ src/utils/mx_which_utils.c
@@ -5,15 +5,15 @@
 #include <ush.h>
 
 int mx_file_exist(char *path) {
-    struct stat *st = (struct stat*) malloc(sizeof (struct stat));
+    struct stat st; 
     int flag = 0;
 
-    if ((lstat(path, st) != -1))
+    if ((lstat(path, &st) != -1))
         flag = 1;
-    if (st != NULL) {
-        free(st);
-        st = NULL;
-    }
+    // if (st != NULL) {
+    //     free(st);
+    //     st = NULL;
+    // }
     return flag;
 }
 

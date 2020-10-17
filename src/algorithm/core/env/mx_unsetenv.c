38feebfc04add9a6cf3af9ead5310bdecaee5b9f
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-10-17 12:56:31 +0300

    ... and for unset too

--- src/algorithm/core/env/mx_unsetenv.c
+++ src/algorithm/core/env/mx_unsetenv.c
@@ -5,8 +5,5 @@
 #include <ush.h>
 
 int mx_unsetenv(const char *s) {
-    t_map *env = mx_env_get();
-    env->remove(env, (char *)s);
-    // TODO: Possible handle error code
-	return 0;
+	return unsetenv(s);
 }

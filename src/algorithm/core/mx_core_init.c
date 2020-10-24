c757b92210a3e4e4b3893b615a66e1071c0dcb4d
Karyna Barinova <kbarinova@e3r5p7.unit.ua>
2020-10-24 09:58:12 +0300

    enabled update_shlvl

--- src/algorithm/core/mx_core_init.c
+++ src/algorithm/core/mx_core_init.c
@@ -7,21 +7,21 @@
 
 // For some reason we don't need it anymore.
 //
-//static void update_shlvl() {
-//    char *shlvl_s = mx_getenv("SHLVL");
-//    if (shlvl_s == 0)
-//        shlvl_s = "0";
-//    int shlvl_i = atoi(shlvl_s);
-//
-//    shlvl_i++;
-//    shlvl_s = mx_itoa(shlvl_i);
-//    mx_setenv("SHLVL", shlvl_s);
-//
-//    mx_strdel(&shlvl_s);
-//}
+static void update_shlvl() {
+   char *shlvl_s = mx_getenv("SHLVL");
+   if (shlvl_s == 0)
+       shlvl_s = "0";
+   int shlvl_i = atoi(shlvl_s);
+
+   shlvl_i++;
+   shlvl_s = mx_itoa(shlvl_i);
+   mx_setenv("SHLVL", shlvl_s);
+
+   mx_strdel(&shlvl_s);
+}
 
 static void init_env() {
-//    update_shlvl();
+   update_shlvl();
 }
 
 static void init_history() {

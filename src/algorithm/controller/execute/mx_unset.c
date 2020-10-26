e5f3b6b17ef66e24b8bd356923f710a6bf285990
Karyna Barinova <kbarinova@e3r3p6.unit.ua>
2020-10-26 14:56:14 +0200

    fixed error output for unset

--- src/algorithm/controller/execute/mx_unset.c
+++ src/algorithm/controller/execute/mx_unset.c
@@ -8,10 +8,16 @@ int mx_unset(char *arguments) {
     if (mx_strlen(arguments)) {
         char **arr = mx_strsplit(arguments, ' ');
         for (int i = 0; i < mx_arr_size(arr); i++) {
-            if (mx_isalpha(arr[i][0]) || arr[i][0] == '_')
+            if (mx_isalpha(arr[i][0]) || arr[i][0] == '_' || mx_isdigit(arr[i][0]))
                 mx_unsetenv(arr[i]);
             else if (mx_strcmp(arr[i], "-") == 0) {
                 mx_printerr("unset: not enough arguments\n");
+                return 1;
+            }
+            else if (mx_strcmp(arr[i], "=") == 0 || mx_strcmp(arr[i], ".") == 0) {
+                mx_printerr("unset: ");
+                mx_printerr(arr[i]);
+                mx_printerr(": invalid parameter name\n");
                 return 1;
             }
             else {

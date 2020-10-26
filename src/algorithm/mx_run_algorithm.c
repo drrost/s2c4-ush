9f60ddadab0a282b18d3fbff87c4cdeb2a0d37fc
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-10-26 22:33:54 +0200

    USH-60 echo "Bye!
    Fixed.

--- src/algorithm/mx_run_algorithm.c
+++ src/algorithm/mx_run_algorithm.c
@@ -3,30 +3,6 @@
 //
 
 #include <ush.h>
-// static void print_input(t_input *main) {
-//
-//     while(main->commands){
-//           mx_printstr("\n");
-//         mx_printstr("data->name: ");
-//         mx_printstr(((t_command *)main->commands->data)->name);
-//         mx_printstr("name");
-//         mx_printstr("\n");
-//         mx_printstr("data->arguments: ");
-//         if (((t_command *)main->commands->data)->arguments != NULL)
-//             mx_printstr(((t_command *)main->commands->data)->arguments);
-//         else
-//             mx_printstr("NULL");
-//         mx_printstr("\n");
-//         mx_printstr("has or: ");
-//         mx_printint((int )((t_command *)main->commands->data)->has_or);
-//         mx_printstr("\n");
-//         mx_printstr("has and: ");
-//         mx_printint((int )((t_command *)main->commands->data)->has_and);
-//         mx_printstr("\n");
-//         mx_printstr("===============================================\n");
-//         main->commands = main->commands->next;
-//     }
-// }
 
 void mx_run_algorithm() {
     bool is_tty = isatty(fileno(stdin));
@@ -40,7 +16,12 @@ void mx_run_algorithm() {
         mx_strdel(&line);
 
         // Execute
-        int result = mx_execute(input);
+        int result = MX_SHOULD_NEXT;
+        if (input->error_text)
+            mx_printerr(input->error_text);
+        else
+            result = mx_execute(input);
+
         mx_input_delete(&input);
 
         // Next
